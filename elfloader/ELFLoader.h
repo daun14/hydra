ca8afe8b147908fcfca1d0fa029f2580925b2397
Ariston Lorenzo <ariston.thomas@daum.net>
2021-03-11 22:45:38 -0500

    ELF: Finished the C to CPP translations :^)
    Signed-off-by: Ariston Lorenzo <ariston.thomas@daum.net>

---
+++ elfloader/ELFLoader.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <../ak/HashMap.h>
+#include <../ak/MappedFile.h>
+#include <../ak/OwnPtr.h>
+#include <../ak/vector.h>
+#include "ExecSpace.h"
+#include "ELFImage.h"
+
+class ELFLoader {
+public:
+    ELFLoader(ExecSpace&, MappedFile&&);
+    ~ELFLoader();
+
+    bool load();
+
+private:
+    void layout();
+    void performRelocations();
+    void exportSymbols();
+    void* lookup(const ELFImage::Symbol&);
+    char* areaForSection(const ELFImage::Section&);
+    char* areaForSectionName(const char*);
+
+    ExecSpace& m_execSpace;
+    HashMap<String, char*> m_sections;
+    OwnPtr<ELFImage> m_image;
+};
---
+++ elfloader/ExecSpace.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <../ak/HashMap.h>
+#include <../ak/MappedFile.h>
+#include <../ak/OwnPtr.h>
+#include <../ak/vector.h>
+
+class ELFLoader;
+
+class ExecSpace {
+public:
+    struct Area {
+        Area(String&& n, char* m, unsigned s)
+            : name(std::move(n))
+            , memory(m)
+            , size(s)
+        {
+        }
+
+        String name;
+        char* memory { 0 };
+        unsigned size { 0 };
+    };
+
+    struct PtrAndSize {
+        PtrAndSize() { }
+        PtrAndSize(char* p, unsigned s)
+            : ptr(p)
+            , size(s)
+        {
+        }
+
+        char* ptr { nullptr };
+        unsigned size { 0 };
+    };
+
+    ExecSpace();
+    ~ExecSpace();
+
+    bool loadELF(MappedFile&&);
+
+    char* symbolPtr(const char* name);
+
+    char* allocateArea(String&& name, unsigned size);
+    void addSymbol(String&& name, char* ptr, unsigned size);
+
+private:
+    void initializeBuiltins();
+
+    Vector<OwnPtr<Area>> m_areas;
+    HashMap<String, PtrAndSize> m_symbols;
+};
